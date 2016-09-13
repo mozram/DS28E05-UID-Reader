3a449af18f97c53b780bd0769c6f7febecea5434
Zulhilmi <soul@outlook.my>
2016-09-13 13:47:20 +0800

    Add project files.

---
+++ DS28E05 ID Reader/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DS28E05_ID_Reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
